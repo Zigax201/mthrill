eac5befdbd610b05d8ecac7799c05dc1426ad8d5
Alvan AlAmien <zigax201@gmail.com>
2021-11-03 21:57:26 +0700

    Initial Commit & Add Auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Illuminate\Support\Facades\Hash;
+use Symfony\Component\HttpFoundation\Response;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'role' => $request->input('role'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return $user;
+    }
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password'))) {
+            return response([
+                'message' => 'Invalid credentials'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = Auth::user();
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $cookie = cookie('jwt', $token, 60 * 24); // 1 day
+
+        return response([
+            'message' => 'Success'
+        ])->withCookie($cookie);
+    }
+
+    public function user()
+    {
+        $user = Auth::user();
+        if ($user->role == 1){
+            return response([
+                'message'=>'Welcome Admin',
+                'profile'=>$user
+            ]);
+        }elseif($user->role == 2){
+            return response([
+                'message'=>'Welcome Customer',
+                'profile'=>$user
+            ]);
+        }
+    }
+
+    public function logout()
+    {
+        $cookie = Cookie::forget('jwt');
+        return response([
+            'message' => 'Success'
+        ])->withCookie($cookie);
+    }
+}
