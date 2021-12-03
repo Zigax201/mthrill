b6ac4486fadcb736a0107aaa2b0ec4e37234278d
Alvan AlAmien <zigax201@gmail.com>
2021-12-03 22:48:09 +0700

    check cart 1.5

--- routes/api.php
+++ routes/api.php
@@ -38,7 +38,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('ongkir', [CheckoutController::class, 'get_ongkir']);
     
     Route::resource('product', ProductController::class);
-    Route::get('cart', [CartController::class, 'cart']);
+    Route::get('carts', [CartController::class, 'cart']);
     Route::post('cart', [CartController::class, 'store_cart']);
     Route::delete('cart', [CartController::class, 'delete_cart']);
     // Route::put('product', [ProductController::class, 'edit']);
