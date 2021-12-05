847b3b5c7903ac8b64a83dd97f9a53dba6d3002f
Alvan AlAmien <zigax201@gmail.com>
2021-12-06 03:22:31 +0700

    fixing picture API 2.8.9

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -113,7 +113,7 @@ class ProductController extends Controller
         //     'message' => 'Success get all picture for this product',
         //     'list_picture' => $list_picture
         // ]);
-        return response()->download(public_path('productphoto/anggrek_pink.jpg'));
+        return response()->download(public_path('anggrek_pink.jpg'));
     }
 
     public function upload_productPicture(Request $request)
