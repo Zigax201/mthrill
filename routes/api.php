2e129f8fc67f2c76dad81aeffe3ddac63e3a21b6
Alvan AlAmien <zigax201@gmail.com>
2021-12-06 04:03:59 +0700

    add delete picture product API

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::resource('product', ProductController::class);
     Route::post('prod/uploadPhoto', [ProductController::class, 'upload_productPicture']);
     Route::get('prod/downloadPhoto', [ProductController::class, 'download_productPicture']);
+    Route::delete('prod/deletePhoto', [ProductController::class, 'delete_productPicture']);
 
     
     Route::post('cart/store', [CartController::class, 'store_cart']);
